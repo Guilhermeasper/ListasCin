ca50859d9d572dba990ba9c54ca249247780d74b
gasp <gasp@cin.ufpe.br>
2018-09-26 12:53:14 -0300

    Teste

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -19,6 +19,20 @@ public class Main {
             }
             n--;
         }
-        for
+        if(contador<4){
+            for(int i = contador; i<5; i++){
+                System.out.print(-1);
+            }
+        }
+        for (int i = v.length; i >= 1; i--) {
+            for (int j = 1; j < i; j++) {
+                if (v[j - 1] > v[j]) {
+                    int aux = v[j];
+                    v[j] = v[j - 1];
+                    v[j - 1] = aux;
+                }
+            }
+        }
+        for(int i = 0; )
     }
 }
