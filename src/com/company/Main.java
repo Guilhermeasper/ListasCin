407dafe3190ad2e4ca34de70caf507dcb942f299
gasp <gasp@cin.ufpe.br>
2018-09-26 12:46:47 -0300

    Teste

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,24 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int n, d, contador = 0;
+        String numeros;
+        int[]  numerosInt = new int[1000];
+        Scanner in = new Scanner(System.in);
+        n = in.nextInt();
+        d = in.nextInt();
+        while(n>0){
+            numeros = in.next();
+            if(numeros.charAt(numeros.length()-1) == (char)d){
+                numerosInt[contador] = Integer.parseInt(numeros);
+                contador++;
+            }
+            n--;
+        }
+        for
     }
 }
